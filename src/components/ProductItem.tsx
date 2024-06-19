b62e68615058fad5f456daab4519002e8a0acee8
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-18 21:47:50 -0300

    Add Tooltip feature to ProductItem component

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -3,6 +3,7 @@ import ShoppingCartPlus from "@/icons/ShoppingCartPlus";
 import Rating from "./Rating";
 import { Button } from "./ui/Button";
 import { Badge } from "./ui/Badge";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/Tooltip";
 
 interface Props {
   product: Product;
@@ -12,17 +13,28 @@ function ProductItem({ product }: Props) {
   return (
     <li>
       <div className="size-full flex flex-col justify-between max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <img className="rounded-t-lg" src={product.thumbnail} alt={`Product image: ${product.title}`} />
+        <img className="rounded-t-lg object-contain max-h-60" src={product.thumbnail} alt={product.description} />
         <div className="px-5 pb-5 flex flex-col justify-between">
-          <a href="#">
-            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{product.title}</h5>
+          <a className="mt-2" href="#">
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <h5 className="text-base font-semibold tracking-tight text-gray-900 dark:text-white text-ellipsis">
+                    {product.title}
+                  </h5>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>{product.title}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           </a>
           <div className="flex items-center mt-2.5 mb-5 gap-2">
             <Rating rating={product.rating} />
             <Badge variant="outline">{product.rating}</Badge>
           </div>
           <div className="flex items-center justify-between">
-            <span className="text-lg text-gray-900 dark:text-white">
+            <span className="text-base text-gray-900 dark:text-white">
               {Intl.NumberFormat("en", {
                 style: "currency",
                 currency: "USD",
